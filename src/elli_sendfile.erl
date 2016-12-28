511bf66d17d1dfe1e98799b0ae55bb2dff5dbb20
Ilya Khaprov <dead.trickster@gmail.com>
2016-12-28 15:09:34 +0300

    rewrite elli_sendfile:sendfile so that it matches file:sendfile

--- src/elli_sendfile.erl
+++ src/elli_sendfile.erl
@@ -3,7 +3,7 @@
 
 -module(elli_sendfile).
 
--export([sendfile/6]).
+-export([sendfile/5]).
 
 -type socket() :: any().
 -type sendfile_opts() :: [{chunk_size, non_neg_integer()}].
@@ -15,30 +15,10 @@
 %% that this function accepts either a raw file or a file name.
 %%
 %% @see file:sendfile/5
--spec sendfile(module(), socket(), file:filename_all() | file:fd(),
+-spec sendfile(file:fd(), socket(),
         non_neg_integer(), non_neg_integer(), sendfile_opts())
     -> {ok, non_neg_integer()} | {error, atom()}.
-sendfile(Transport, Socket, Filename, Offset, Bytes, Opts)
-        when is_list(Filename) orelse is_atom(Filename)
-        orelse is_binary(Filename) ->
-    ChunkSize = chunk_size(Opts),
-    case file:open(Filename, [read, raw, binary]) of
-        {ok, RawFile} ->
-            _ = case Offset of
-                0 ->
-                    ok;
-                _ ->
-                    {ok, _} = file:position(RawFile, {bof, Offset})
-            end,
-            try
-                sendfile_loop(Transport, Socket, RawFile, Bytes, 0, ChunkSize)
-            after
-                ok = file:close(RawFile)
-            end;
-        {error, _Reason} = Error ->
-            Error
-    end;
-sendfile(Transport, Socket, RawFile, Offset, Bytes, Opts) ->
+sendfile(RawFile, Socket,Offset, Bytes, Opts) ->
     ChunkSize = chunk_size(Opts),
     Initial2 = case file:position(RawFile, {cur, 0}) of
         {ok, Offset} ->
@@ -47,7 +27,7 @@ sendfile(Transport, Socket, RawFile, Offset, Bytes, Opts) ->
             {ok, _} = file:position(RawFile, {bof, Offset}),
             Initial
         end,
-    case sendfile_loop(Transport, Socket, RawFile, Bytes, 0, ChunkSize) of
+    case sendfile_loop(Socket, RawFile, Bytes, 0, ChunkSize) of
         {ok, _Sent} = Result ->
             {ok, _} = file:position(RawFile, {bof, Initial2}),
             Result;
@@ -67,21 +47,21 @@ chunk_size(Opts) ->
             16#1FFF
     end.
 
--spec sendfile_loop(module(), socket(), file:fd(), non_neg_integer(),
+-spec sendfile_loop(socket(), file:fd(), non_neg_integer(),
         non_neg_integer(), pos_integer())
     -> {ok, non_neg_integer()} | {error, term()}.
-sendfile_loop(_Transport, _Socket, _RawFile, Sent, Sent, _ChunkSize)
+sendfile_loop(_Socket, _RawFile, Sent, Sent, _ChunkSize)
         when Sent =/= 0 ->
     %% All requested data has been read and sent, return number of bytes sent.
     {ok, Sent};
-sendfile_loop(Transport, Socket, RawFile, Bytes, Sent, ChunkSize) ->
+sendfile_loop(Socket, RawFile, Bytes, Sent, ChunkSize) ->
     ReadSize = read_size(Bytes, Sent, ChunkSize),
     case file:read(RawFile, ReadSize) of
         {ok, IoData} ->
-            case Transport:send(Socket, IoData) of
+            case ssl:send(Socket, IoData) of
                 ok ->
                     Sent2 = iolist_size(IoData) + Sent,
-                    sendfile_loop(Transport, Socket, RawFile, Bytes, Sent2,
+                    sendfile_loop(Socket, RawFile, Bytes, Sent2,
                         ChunkSize);
                 {error, _Reason} = Error ->
                     Error
