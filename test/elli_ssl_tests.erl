5c1edc263c0e5329c625d63fa4f5ed791b68870a
Ilya Khaprov <dead.trickster@gmail.com>
2016-12-28 13:35:31 +0300

    add sendfile test to ssl_tests

--- test/elli_ssl_tests.erl
+++ test/elli_ssl_tests.erl
@@ -2,13 +2,27 @@
 -include_lib("eunit/include/eunit.hrl").
 -include("elli_test.hrl").
 
+-define(README, "README.md").
+
 elli_ssl_test_() ->
     {setup,
      fun setup/0, fun teardown/1,
-     [
-      ?_test(hello_world()),
-      ?_test(chunked())
+     [{foreach,
+       fun init_stats/0, fun clear_stats/1,
+       [
+        ?_test(hello_world()),
+        ?_test(chunked()),
+        ?_test(sendfile())
+       ]}
      ]}.
+
+get_size_value(Key) ->
+    [{sizes, Sizes}] = ets:lookup(elli_stat_table, sizes),
+    proplists:get_value(Key, Sizes).
+
+get_timing_value(Key) ->
+    [{timings, Timings}] = ets:lookup(elli_stat_table, timings),
+    proplists:get_value(Key, Timings).
 
 %%% Tests
 
@@ -29,6 +43,31 @@ chunked() ->
                   {"content-type", "text/event-stream"}], headers(Response)),
     ?assertMatch(Expected, body(Response)).
 
+sendfile() ->
+    {ok, Response} = httpc:request("https://localhost:3443/sendfile"),
+    F              = ?README,
+    {ok, Expected} = file:read_file(F),
+
+    ?assertMatch(200, status(Response)),
+    ?assertEqual([{"connection", "Keep-Alive"},
+                  {"content-length", integer_to_list(size(Expected))}],
+                 headers(Response)),
+    ?assertEqual(binary_to_list(Expected), body(Response)),
+    %% sizes
+    ?assertEqual(size(Expected), get_size_value(file)),
+    ?assertMatch(65, get_size_value(resp_headers)),
+    %% timings
+    ?assertNotMatch(undefined, get_timing_value(request_start)),
+    ?assertNotMatch(undefined, get_timing_value(headers_start)),
+    ?assertNotMatch(undefined, get_timing_value(headers_end)),
+    ?assertNotMatch(undefined, get_timing_value(body_start)),
+    ?assertNotMatch(undefined, get_timing_value(body_end)),
+    ?assertNotMatch(undefined, get_timing_value(user_start)),
+    ?assertNotMatch(undefined, get_timing_value(user_end)),
+    ?assertNotMatch(undefined, get_timing_value(send_start)),
+    ?assertNotMatch(undefined, get_timing_value(send_end)),
+    ?assertNotMatch(undefined, get_timing_value(request_end)).
+
 %%% Internal helpers
 
 setup() ->
@@ -43,12 +82,21 @@ setup() ->
     CertFile = filename:join(CertDir, "server_cert.pem"),
     KeyFile  = filename:join(CertDir, "server_key.pem"),
 
+    Config = [
+              {mods, [
+                      {elli_metrics_middleware, []},
+                      {elli_middleware_compress, []},
+                      {elli_example_callback, []}
+                     ]}
+             ],
+
     {ok, P}  = elli:start_link([
                                 {port, 3443},
                                 ssl,
                                 {keyfile, KeyFile},
                                 {certfile, CertFile},
-                                {callback, elli_example_callback}
+                                {callback,  elli_middleware},
+                                {callback_args, Config}
                                ]),
     unlink(P),
     [P].
@@ -59,3 +107,9 @@ teardown(Pids) ->
     application:stop(public_key),
     application:stop(crypto),
     [elli:stop(P) || P <- Pids].
+
+init_stats() ->
+    ets:new(elli_stat_table, [set, named_table, public]).
+
+clear_stats(_) ->
+    ets:delete(elli_stat_table).
